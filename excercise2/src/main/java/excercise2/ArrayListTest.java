f9749b9e9856ce6e4beef89e8cb5b3015fda71a8
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2018-10-11 21:47:53 +0200

    ;)

---
+++ excercise2/src/main/java/excercise2/ArrayListTest.java
@@ -0,0 +1,17 @@
+package excercise2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        // LinkedList<String> b = new LinkedList<>();
+        // Collection c = new ArrayList();
+
+        // a.add("HelloWorld!");
+
+        a.contains("1");
+    }
+}
--- excercise2/src/main/java/excercise2/MyMainClass.java
+++ excercise2/src/main/java/excercise2/MyMainClass.java
@@ -14,6 +14,13 @@ import soot.jimple.toolkits.ide.icfg.JimpleBasedInterproceduralCFG;
 import soot.options.Options;
 import soot.toolkits.scalar.Pair;
 
+import heros.DefaultSeeds;
+import heros.FlowFunction;
+import heros.FlowFunctions;
+import heros.InterproceduralCFG;
+import heros.flowfunc.Identity;
+import heros.flowfunc.KillAll;
+
 public class MyMainClass {
 
 	public static final boolean DEBUG = true;
@@ -119,7 +126,7 @@ public class MyMainClass {
 		}
 	}
 
-	public static class MyIFDSTabulator extends DefaultJimpleIFDSTabulationProblem<Set<InvokeExpr>, InterproceduralCFG<Unit, SootMethod>> {
+	public static class MyIFDSTabulator extends DefaultJimpleIFDSTabulationProblem<Pair<Local, Set<InvokeExpr>>, InterproceduralCFG<Unit, SootMethod>> {
 		/**
 		 * Cosntructs an IFDS tabulator
 		 * @param icfg
@@ -129,33 +136,50 @@ public class MyMainClass {
 		}
 
 		@Override
-		public FlowFunctions<Unit, Set<InvokeExpr>, SootMethod> createFlowFunctionsFactory() {
-			return new FlowFunctions<Unit, Set<InvokeExpr>, SootMethod>() {
+		public FlowFunctions<Unit, Pair<Local, Set<InvokeExpr>>, SootMethod> createFlowFunctionsFactory() {
+			System.out.println("createFlowFunctionsFactory");
+
+			return new FlowFunctions<Unit, Pair<Local, Set<InvokeExpr>>, SootMethod>() {
 
 				@Override
-				public FlowFunction<Set<InvokeExpr>> getNormalFlowFunction(final Unit curr, Unit succ) {
-					if (curr instanceof DefinitionStmt) {
-						final DefinitionStmt assignment = (DefinitionStmt) curr;
+				public FlowFunction<Pair<Local, Set<InvokeExpr>>> getNormalFlowFunction(final Unit curr, Unit succ) {
+					// System.out.println("getNormalFlowF: " + curr.toString());
 
-						return new FlowFunction<Set<InvokeExpr>>() {
+					if (curr instanceof InvokeExpr) {
+						final InvokeExpr invk = (InvokeExpr) curr;
+
+						// System.out.println("getNormalFlowF invoked: " + invk.getMethod().getName());
+/*
+						return new FlowFunction<Pair<Local, Set<InvokeExpr>>>() {
 							@Override
-							public Set<Set<InvokeExpr>> computeTargets(Set<InvokeExpr> source) {
+							public Set<Pair<Local, Set<InvokeExpr>>> computeTargets(Pair<Local, Set<InvokeExpr>> source) {
+								System.out.println("getNormalFlowF computeTargets: " + curr.toString());
 								return null;
 							}
 						};
+					*/
 					}
 
 					return Identity.v();
 				}
 
 				@Override
-				public FlowFunction<Set<InvokeExpr>> getCallFlowFunction(Unit callStmt,
+				public FlowFunction<Pair<Local, Set<InvokeExpr>>> getCallFlowFunction(Unit callStmt,
 																		 final SootMethod destinationMethod) {
-					/*
+					// System.out.println("getCallFlowF: " + callStmt.toString());
+
 					Stmt stmt = (Stmt) callStmt;
 					InvokeExpr invokeExpr = stmt.getInvokeExpr();
-					final List<Value> args = invokeExpr.getArgs();
+					// final List<Value> args = invokeExpr.getArgs();
 
+					if (!stmt.getDefBoxes().isEmpty()) {
+						Value v = stmt.getDefBoxes().get(0).getValue();
+						if (v instanceof Local) {
+							// System.out.println("v is local: " + v.toString());
+						}
+					}
+
+					/*
 					final List<Local> localArguments = new ArrayList<Local>(args.size());
 					for (Value value : args) {
 						if (value instanceof Local) {
@@ -166,28 +190,40 @@ public class MyMainClass {
 					}
 					*/
 
-					return new FlowFunction<Set<InvokeExpr>>() {
+					// InvokeExpr[] invk = new InvokeExpr[]{ invokeExpr };
+
+					return Identity.v();
+					/*
+					return new FlowFunction<Pair<Unit, Set<InvokeExpr>>>() {
 						@Override
-						public Set<Set<InvokeExpr>> computeTargets(Set<InvokeExpr> source) {
+						public Set<Pair<Unit, Set<InvokeExpr>>> computeTargets(Pair<Unit, Set<InvokeExpr>> source) {
+							System.out.println("getCallFlowF computeTargets: " + callStmt.toString());
 							return null;
 						}
 					};
+					*/
 				}
 
 				@Override
-				public FlowFunction<Set<InvokeExpr>> getReturnFlowFunction(final Unit callSite,
-																							SootMethod calleeMethod, final Unit exitStmt, Unit returnSite) {
-					return new FlowFunction<Set<InvokeExpr>>() {
+				public FlowFunction<Pair<Local, Set<InvokeExpr>>> getReturnFlowFunction(final Unit callSite,
+																		   SootMethod calleeMethod, final Unit exitStmt, Unit returnSite) {
+					//	System.out.println("getReturnFlowF: " + callSite.toString());
+
+					return Identity.v();
+					/*
+					return new FlowFunction<Pair<Unit, Set<InvokeExpr>>>() {
 
 						@Override
-						public Set<Set<InvokeExpr>> computeTargets(Set<InvokeExpr> source) {
+						public Set<Pair<Unit, Set<InvokeExpr>>> computeTargets(Pair<Unit, Set<InvokeExpr>> source) {
+							System.out.println("getReturnFlowF computeTargets: " + callSite.toString());
 							return null;
 						}
 					};
+					*/
 				}
 
 				@Override
-				public FlowFunction<Set<InvokeExpr>> getCallToReturnFlowFunction(Unit callSite, Unit returnSite) {
+				public FlowFunction<Pair<Local, Set<InvokeExpr>>> getCallToReturnFlowFunction(Unit callSite, Unit returnSite) {
 					/*
 					if (!(callSite instanceof DefinitionStmt)) { // TODO: LinkedList/ArrayList
 						return Identity.v();
@@ -196,26 +232,53 @@ public class MyMainClass {
 					final DefinitionStmt definitionStmt = (DefinitionStmt) callSite;
 					*/
 
-					return new FlowFunction<Set<InvokeExpr>>() {
+					//	System.out.println("getCallToReturnFlowF: " + callSite.toString());
 
+					if (callSite instanceof InvokeExpr) {
+						// System.out.println("Found InvokeExpr: " + callSite.toString());
+					}
+
+					return Identity.v();
+
+					/*
+					return new FlowFunction<Pair<Unit, Set<InvokeExpr>>>() {
 						@Override
-						public Set<Set<InvokeExpr>> computeTargets(Set<InvokeExpr> source) {
+						public Set<Pair<Unit, Set<InvokeExpr>>> computeTargets(Pair<Unit, Set<InvokeExpr>> source) {
+							System.out.println("getCallToReturnFlowF computeTargets: " + callSite.toString());
 							return null;
 						}
 					};
+					*/
 				}
 			};
 		}
 
 		@Override
-		public Set<InvokeExpr> createZeroValue() {
-			return new HashSet<>();
+		public Map<Unit, Set<Pair<Local, Set<InvokeExpr>>>> initialSeeds() {
+			Map<Unit, Set<Pair<Local, Set<InvokeExpr>>>> map = new LinkedHashMap<>();
+			Set<Pair<Local, Set<InvokeExpr>>> pairs = new LinkedHashSet<>();
+
+			for (Local l : Scene.v().getMainMethod().getActiveBody().getLocals()) {
+				if (l.getType().toString().equals("java.util.ArrayList") ||
+					l.getType().toString().equals("java.util.LinkedList")) {
+					System.out.println("initialSeeds, adding: " + l.getType().toString() + " var: " + l.getName());
+					pairs.add(new Pair<>(l, new LinkedHashSet<>()));
+				}
+			}
+
+			for (Unit u : Scene.v().getMainMethod().getActiveBody().getUnits()) {
+				map.put(u, new LinkedHashSet<>(pairs));
+			}
+
+			// return DefaultSeeds.make(Scene.v().getMainMethod().getActiveBody().getUnits(), zeroValue());
+			return map;
 		}
 
 		@Override
-		public Map<Unit, Set<Set<InvokeExpr>>> initialSeeds() {
-			return null;
+		public Pair<Local, Set<InvokeExpr>> createZeroValue() {
+			return new Pair<>(new JimpleLocal("<<zero>>", NullType.v()), Collections.emptySet());
 		}
+
 	}
 
 	public static final class MySceneTransformer extends SceneTransformer {
@@ -225,18 +288,29 @@ public class MyMainClass {
 			// This call will raise an ex
 			SootMethod m = Scene.v().getMethod("<java.util.Collection: boolean contains(java.lang.Object)>");
 
-			/*
-			IFDSTabulationProblem<Unit, Set<InvokeExpr>, SootMethod, InterproceduralCFG<Unit, SootMethod>> problem =
-					new MyIFDSTabulator(new JimpleBasedInterproceduralCFG());
-			JimpleIFDSSolver<Set<InvokeExpr>, InterproceduralCFG<Unit, SootMethod>> solver = new JimpleIFDSSolver<>(problem);
+			InterproceduralCFG<Unit, SootMethod> icfg = new JimpleBasedInterproceduralCFG(false, false);
+			IFDSTabulationProblem<Unit, Pair<Local, Set<InvokeExpr>>, SootMethod, InterproceduralCFG<Unit, SootMethod>> problem =
+					new MyIFDSTabulator(icfg);
+			//IFDSTabulationProblem<Unit, Pair<Value, Set<DefinitionStmt>>, SootMethod, InterproceduralCFG<Unit, SootMethod>> problem =
+			//		new IFDSReachingDefinitions(icfg);
+
+			JimpleIFDSSolver<Pair<Local, Set<InvokeExpr>>, InterproceduralCFG<Unit, SootMethod>> solver = new JimpleIFDSSolver<>(problem);
+			// JimpleIFDSSolver<Pair<Value, Set<DefinitionStmt>>, InterproceduralCFG<Unit, SootMethod>> solver = new JimpleIFDSSolver<>(problem);
 			solver.solve();
-			*/
+			solver.printStats();
+			solver.dumpResults();
+			System.out.println("Propagation count: " + solver.propagationCount);
+
+			// icfg.getCallersOf(m);
+
+			/*
 			printAll(new Function<Stmt, String>() {
 				@Override
 				public String apply(Stmt stmt) {
 					return null;
 				}
 			});
+			*/
 		}
 	}
 }
